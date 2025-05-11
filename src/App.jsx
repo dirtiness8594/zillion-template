c945b044f0475819da5e085f68bcc822bace4615
themandalorian <dwarfoffroad@protonmail.com>
2025-05-11 12:44:32 -0300

    feat/adding_basic_conf_helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
+
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -10,19 +12,26 @@ import Workflow from './components/Workflow';
 import Footer from './components/Footer';
 import WhatsappButton from './components/WhatsappButton';
 
-function App() {  
+function App() {
 
     return (
         <>
-        <Header />
-        <Hero />
-        <About />
-        <Workflow />
-        <Services />
-        <Values />
-        <Contact />
-        <Footer />
-        <WhatsappButton />
+            <Helmet>
+                <title>Produto Incrível | Minha Loja</title>
+                <meta name="description" content="Compre o Produto Incrível com desconto!" />
+                <meta property="og:title" content="Produto Incrível | Minha Loja" />
+                <meta property="og:description" content="Oferta imperdível do Produto Incrível." />
+                <meta property="og:image" content="https://meusite.com/imagens/produto.jpg" />
+            </Helmet>
+            <Header />
+            <Hero />
+            <About />
+            <Workflow />
+            <Services />
+            <Values />
+            <Contact />
+            <Footer />
+            <WhatsappButton />
         </>
     );
 }
