5d978e79251d8b4d689e82838ce3a96a64c93837
themandalorian <dwarfoffroad@protonmail.com>
2025-05-11 12:45:19 -0300

    feat/adding_tabs_component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,8 @@ import { CiStickyNote } from "react-icons/ci";
 import { LuLampDesk } from "react-icons/lu";
 import { TfiTarget } from "react-icons/tfi";
 import { FaArrowRight } from "react-icons/fa";
+import { MyTab, Tab, Content } from 'tab-react-simple';
+
 
 const About = () => {
 
@@ -14,7 +16,27 @@ const About = () => {
             </p>
 
             <div className="sobre__mvv">
-                <div className="sobre__card">
+
+                <MyTab>
+                    <Tab><h3><CiStickyNote /> Visão <FaArrowRight /></h3></Tab>
+                    <Content><p>
+                        Conectando Empresas Para Gerar Prosperidade Local. Na Zillion Business Center, vislumbramos um ecossistema empresarial vibrante e próspero no Triângulo Mineiro. O sucesso de uma empresa não é uma jornada solitária — é a soma de colaborações estratégicas, trocas de experiências e apoio mútuo.
+                    </p></Content>
+                    <Tab><h3><TfiTarget /> Missão <FaArrowRight /></h3></Tab>
+                    <Content><p>
+                        Construir Pontes para o Futuro Empresarial Sustentável. Nossa missão é conectar empreendedores visionários a oportunidades valiosas e recursos cruciais, oferecendo um ambiente propício à inovação, aprendizado e crescimento — onde startups e empresas maduras possam florescer.
+                    </p></Content>
+                    <Tab><h3><LuLampDesk /> Valores <FaArrowRight /></h3></Tab>
+                    <Content><ul>
+                        <li>Ética e transparência</li>
+                        <li>Colaboração acima da competição</li>
+                        <li>Fomento à inovação</li>
+                        <li>Compromisso com resultados sustentáveis</li>
+                        <li>Responsabilidade social e regional</li>
+                    </ul></Content>
+                </MyTab>
+
+                {/* <div className="sobre__card">
                     <h3><CiStickyNote /> Visão <FaArrowRight /></h3>
                     <p>
                         Conectando Empresas Para Gerar Prosperidade Local. Na Zillion Business Center, vislumbramos um ecossistema empresarial vibrante e próspero no Triângulo Mineiro. O sucesso de uma empresa não é uma jornada solitária — é a soma de colaborações estratégicas, trocas de experiências e apoio mútuo.
@@ -37,7 +59,7 @@ const About = () => {
                         <li>Compromisso com resultados sustentáveis</li>
                         <li>Responsabilidade social e regional</li>
                     </ul>
-                </div>
+                </div> */}
             </div>
         </section>
     );
